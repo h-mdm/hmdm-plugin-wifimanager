77131bfbd412970b0c3001756f1e57e61caca7df
Lukáš Eret <33234640+eretl@users.noreply.github.com>
2026-03-04 23:52:47 +0000

    added comment

--- app/src/main/java/com/hmdm/wifimanager/model/HiddenWiFiItem.java
+++ app/src/main/java/com/hmdm/wifimanager/model/HiddenWiFiItem.java
@@ -72,6 +72,7 @@ public class HiddenWiFiItem extends WiFiItem {
     @Override
     public String getCapabilities() {
         if (scanResult == null) {
+            //ScanResult.capabilities are conventionally wrapped by [ ], so a raw string may not work as expected
             if (security != null && !security.isEmpty() && !security.startsWith("[")) {
                 return "[" + security + "]";
             }
